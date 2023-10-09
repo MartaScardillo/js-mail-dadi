c41a79ee95d10e4701703afbb1f6341516c51a2d
Marta Scardillo <martascardillo02@gmail.com>
2023-10-09 19:45:47 +0200

    Add rounds

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 const arrayEmails = [
-  "Harry.Potter1@gmail.com", // i=0 arrayEmails[i] => arrayEmails[0]
-  "DracoMalfoy_@gmail.com", // i=1 arrayEmails[i] => arrayEmails[1]
-  "a", // i=2 arrayEmails[i] => arrayEmails[2]
+  "FrodoBaggins1@gmail.com", // i=0 arrayEmails[i] => arrayEmails[0]
+  "Gandalf_@gmail.com", // i=1 arrayEmails[i] => arrayEmails[1]
+  "Gollum@gmail.com", // i=2 arrayEmails[i] => arrayEmails[2]
 ];
 
 const lenghtArray = arrayEmails.length;
 
 let isEmailFound = false;
 
-let userEmail = prompt("Email");
+let userEmail = prompt("Inserisci la tua email");
+console.log("Benvenuto " + userEmail + "!");
 
 for (let i = 0; i < lenghtArray; i++) {
   let mail = arrayEmails[i];
-  console.log(mail);
 
   if (userEmail === mail) {
     isEmailFound = true;
@@ -20,23 +20,56 @@ for (let i = 0; i < lenghtArray; i++) {
 }
 
 if (isEmailFound === true) {
-  console.log("accesso consentito");
+  console.log("Buon Game!\n");
+  let round = 1;
+  let userPoints = 0,
+    botPoints = 0,
+    userWin;
 
-  for (let i = 0; i < 1; i++) {
-    let userRandomNumber = Math.floor(Math.random() * 6) + 1
-    console.log(userEmail + ' ha tirato il dado, ed è uscito... ' + userRandomNumber + '!');
-    let botRandomNumber = Math.floor(Math.random() * 6) + 1
-    console.log('Il tuo computer ha tirato il dado, ed è uscito... ' + botRandomNumber + '!');
+  for (userPoints = 0, botPoints = 0; userPoints < 2 && botPoints < 2; ) {
+    let userRandomNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(
+      "\n\nROUND: " +
+        round +
+        "\n" +
+        "Hai tirato il dado, ed è uscito... " +
+        userRandomNumber +
+        "!"
+    );
+
+    let botRandomNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(
+      "Il tuo computer ha tirato il dado, ed è uscito... " +
+        botRandomNumber +
+        "!"
+    );
 
     if (userRandomNumber > botRandomNumber) {
-        console.log('hai vinto il round ' + i '!');
-    } else if (userRandomNumber < botRandomNumber){
-        console.log('Hai perso il round ' + i '!');
+      console.log("Hai vinto il round " + round + "!");
+      userPoints++;
+      userWin = true;
+    } else if (userRandomNumber < botRandomNumber) {
+      console.log("Hai perso il round " + round + "!");
+      botPoints++;
+      userWin = false;
+    } else {
+      console.log("Pareggio!");
     }
+
+    console.log(
+      "\nIl tuo punteggio è: " +
+        userPoints +
+        "\nIl punteggio del tuo computer è: " +
+        botPoints
+    );
+    round++;
   }
 
-
+  if (userWin) {
+    console.log("HAI VINTO!");
+  } else {
+    console.log("HAI PERSO!");
+  }
 } else {
-  console.log("accesso negato");
+  console.log("TUUU NON PUOIIII PASSAREEEEEEEEEE!");
 }
-
